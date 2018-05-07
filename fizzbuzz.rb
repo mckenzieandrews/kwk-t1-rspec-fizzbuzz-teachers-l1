e2707f8662988abfc815da055ea2827378d4e6d6
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-05-07 21:17:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,18 @@
 def fizzbuzz(int)
 
-  if int % 3 == 0 && int % 5 != 0
-    "Fizz"
-  end
-
-  if int % 5 == 0 && int % 3 != 0
-    "Buzz"
-  end
 
   if int %3 == 0 && int % 5 == 0
     "FizzBuzz"
+    
+  elsif int % 3 == 0 
+    "Fizz"
+
+
+  elsif int % 5 == 0 
+    "Buzz"
+
+
+  
   end
 
 end
